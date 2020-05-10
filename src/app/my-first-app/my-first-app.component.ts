fcf9d842f13f80aebf8974b808a9eaa572d99ed5
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2020-05-11 00:11:10 +0530

    comments added

--- src/app/my-first-app/my-first-app.component.ts
+++ src/app/my-first-app/my-first-app.component.ts
@@ -3,7 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-my-first-app',
   templateUrl: './my-first-app.component.html',
+  //inline template example
+  //template:`<app-my-first-app></app-my-first-app>`
+  
   styleUrls: ['./my-first-app.component.css']
+  //inline style example
+  // style:`h1{color:blue}`
 })
 export class MyFirstAppComponent implements OnInit {
 
