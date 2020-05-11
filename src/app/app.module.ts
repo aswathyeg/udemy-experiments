d271fbd5738786adb674185696316cbcaa4a97b3
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2020-05-12 01:21:27 +0530

    added forms module to use ngModel (2 way binding)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-//import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MyFirstAppComponent } from './my-first-app/my-first-app.component';
@@ -12,7 +12,7 @@ import { MyFirstAppComponent } from './my-first-app/my-first-app.component';
   ],
   imports: [
     BrowserModule,
-    //FormsModule
+    FormsModule
 
   ],
   providers: [],
