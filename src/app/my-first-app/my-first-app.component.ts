a48dee6a4ec9ff1cf26ba29f82a6d122794e58bd
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2020-05-13 01:23:58 +0530

    directives examples

--- src/app/my-first-app/my-first-app.component.ts
+++ src/app/my-first-app/my-first-app.component.ts
@@ -14,20 +14,27 @@ export class MyFirstAppComponent implements OnInit {
 allowmyfirstapp=false;
 clickme="not clicked";
 username='';
+names=[];
+paragraph=false;
 
 
-  constructor() { setInterval(()=>{
-    this.allowmyfirstapp=true;},2000);
+  //constructor() { setInterval(()=>{
+    //this.allowmyfirstapp=true;},2000);
 
-  }
+  //}
   
   ngOnInit() {
   }
   onClick(){
     
     this.clickme='clicked';
+    this.names.push(this.username)
   }
   //reset(){
     //this.username=="''";
 //}
+onDisplay(){
+  this.paragraph=!this.paragraph;
+  this.names.push(this.names.length+1)
+}
 }
