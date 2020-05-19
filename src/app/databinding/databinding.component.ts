493a7a0e7e9e4950693d89e4f1862eb4c778e716
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2020-05-20 02:08:03 +0530

    added methods

---
+++ src/app/databinding/databinding.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-databinding',
+  templateUrl: './databinding.component.html',
+  styleUrls: ['./databinding.component.css']
+})
+export class DatabindingComponent implements OnInit {
+  servername='';
+  servercontent='';
+  elements=[];
+  content='';
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onAddserver(){
+
+
+  }
+
+  onAddserverblueprint(){
+
+  }
+
+}
