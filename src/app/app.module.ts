54334bafe46ae1368f14ef16d55b9ef060bcfe55
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2020-05-22 22:14:50 +0530

    renamed component name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { MyFirstAppComponent } from './my-first-app/my-first-app.component';
 import { DatabindingComponent } from './databinding/databinding.component';
 import { ServerElementComponent } from './server-element/server-element.component';
-import { InterpolationComponent } from './interpolation/interpolation.component';
+import { InterpolationComponent } from './databinding-alltypes/interpolation.component';
 
 @NgModule({
   declarations: [
